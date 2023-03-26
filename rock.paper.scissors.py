ed74354421afd78442e0d447297f587640024d6c
Rares <mulea.rares@gmail.com>
2023-03-26 18:25:18 +0300

    Update rock.paper.scissors.py

--- rock.paper.scissors.py
+++ rock.paper.scissors.py
@@ -1,36 +1,36 @@
-# import random
-# #helps the computer import a random, in this case, option
-# print("Rock", "Paper", "Scissors",":","The game")
-# #prints the name of the game
-# options = ["rock", "paper", "scissors"]
-#
-# while True:
-# # a loop that will repeat as long as it's condition/s are true
-#     user_choice = input("Enter you choice: ").lower()
-#     #asking the user to input a choice. The lower method transforms the choice of the user in lower characters,
-#     #if the input is with big letters
-#     if user_choice not in options:
-#         print("Your choice is not an option. You should choose between: rock, paper, scissors!")
-#         continue
-#     #checking if the users choice is in the options list
-#     computer_choice = random.choice(options)
-#     #this is were the random module takes effect
-#
-#     #checking who won
-#     if user_choice == computer_choice:
-#         print("It's a tie")
-#     elif user_choice == "rock" and computer_choice == "scissors":
-#         print("You win! Rock beats scissors.")
-#     elif user_choice == "scissors" and computer_choice == "paper":
-#         print("You win! Scissors beats paper.")
-#     elif user_choice == "paper" and computer_choice == "rock":
-#         print("You win! Paper beats rock.")
-#     else:
-#         print("You lose, {} beats {}.".format(computer_choice, user_choice))
-#     #the format() method formats the specified value(s) and insert them inside the string's placeholder
-#     #the first curly bracket is the computer_choice and the second is the user_choice
-#     play_again = input("Do you want to play again? (yes/no) ").lower()
-#     if play_again != "yes":
-#         break
-#
-# print("Thanks for playing!")
+ import random
+ #helps the computer import a random, in this case, option
+ print("Rock", "Paper", "Scissors",":","The game")
+ #prints the name of the game
+ options = ["rock", "paper", "scissors"]
+
+ while True:
+ # a loop that will repeat as long as it's condition/s are true
+     user_choice = input("Enter you choice: ").lower()
+     #asking the user to input a choice. The lower method transforms the choice of the user in lower characters,
+     #if the input is with big letters
+     if user_choice not in options:
+         print("Your choice is not an option. You should choose between: rock, paper, scissors!")
+         continue
+     #checking if the users choice is in the options list
+     computer_choice = random.choice(options)
+     #this is were the random module takes effect
+
+     #checking who won
+     if user_choice == computer_choice:
+         print("It's a tie")
+     elif user_choice == "rock" and computer_choice == "scissors":
+         print("You win! Rock beats scissors.")
+     elif user_choice == "scissors" and computer_choice == "paper":
+         print("You win! Scissors beats paper.")
+     elif user_choice == "paper" and computer_choice == "rock":
+         print("You win! Paper beats rock.")
+     else:
+         print("You lose, {} beats {}.".format(computer_choice, user_choice))
+     #the format() method formats the specified value(s) and insert them inside the string's placeholder
+     #the first curly bracket is the computer_choice and the second is the user_choice
+    play_again = input("Do you want to play again? (yes/no) ").lower()
+     if play_again != "yes":
+         break
+
+ print("Thanks for playing!")
